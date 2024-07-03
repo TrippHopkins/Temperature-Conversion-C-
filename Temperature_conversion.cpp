0c757b506e066b185a67378231251aa773da9a59
Tripp Hopkins <tripphopkins13245@gmail.com>
2024-07-03 13:53:13 -0400

    Added Conversion Code

--- Temperature_conversion.cpp
+++ Temperature_conversion.cpp
@@ -2,6 +2,34 @@
 
 int main(){
 
+    double temp;
+    char unit;
+
+    std::cout << "******************** temp conversion ********************";
+    std::cout << "F = Fahrenheit\n";
+    std::cout << "C = Celsius\n";
+    std::cout << "what unit would you like to convert to: ";
+    std::cin >> unit;
+    
+    if(unit == 'F' || unit == 'f'){
+        std::cout << "Enter celsius temp: ";
+        std::cin >> temp;
+
+        temp = (1.8 * temp) + 32;
+        std::cout << "temp is: " << temp << "F\n";
+    }
+    else if(unit == 'C' || unit == 'c'){
+        std::cout << "Enter Fahrenheit temp: ";
+        std::cin >> temp;
+
+        temp = (temp - 32) / 1.8;
+        std::cout << "temp is: " << temp << "C\n";
+    }
+    else{
+        std::cout << "please enter in only C or F\n";
+    }
+    
+    std::cout << "*********************************************************";
 
 
     return 0;
